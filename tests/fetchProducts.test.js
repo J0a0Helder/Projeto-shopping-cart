82edba5a96fd69ead3e619de524cf342aee9bd0a
João H <joaoheldermartins@hotmail.com>
2022-08-09 15:23:14 -0300

    requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,27 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchProducts é uma função.', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Verifica se a função fetch foi chamada.', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se a função fetch foi chamada com o endpoint correto', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('Verifica se o retorno da função fetchProducts é uma estrutura de dados', async () => {
+    const result = await fetchProducts('computador');
+    expect(result).toEqual(computadorSearch);
+  });
+
+  it('Verifica se retorna um erro se a função fetchProducts for chamada sem argumentos', async () => {
+    const result = await fetchProducts();
+    expect(result).toEqual( new Error('You must provide an url'));
+  });
 });

3f200ca4b4e0c637f051aeb278dbf0e2cc06b086
João H <joaoheldermartins@hotmail.com>
2022-08-09 20:02:50 -0300

    requisito 5 corrigindo lint

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const createCustomElement = (element, className, innerText) => {
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
+const cartItemClickListener = (event) => {
+  event.target.remove();
+};
+
 const createCartItemElement = ({ sku, name, salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -64,10 +68,6 @@ const productList = async () => {
     const elements = createProductItemElement(products);
     itemsContainer.appendChild(elements);  
   });
-};
-
-const cartItemClickListener = (event) => {
- event.target.remove();
 };
 
 window.onload = productList;
